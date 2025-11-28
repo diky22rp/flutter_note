90b1d3085dcb059d2e561eeb3a490f446a164955
Diky Rizki Pratama <diky.pratama9@gmail.com>
2025-11-28 20:09:56 +0700

    update firestore helper

--- lib/firestore_helper.dart
+++ lib/firestore_helper.dart
@@ -1,28 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_note/models/note_model.dart';
 
 class FirestoreHelper {
-  final noteRef = FirebaseFirestore.instance
+  final _currentUserId = FirebaseAuth.instance.currentUser?.uid;
+
+  late final _currentUserRef = FirebaseFirestore.instance
+      .collection("users_note")
+      .doc(_currentUserId)
       .collection("notes")
-      .withConverter<NoteModel>(
-        fromFirestore: (snapshot, _) {
-          final data = snapshot.data()!;
-          data["note_id"] ??= snapshot.id;
-          return NoteModel.fromJson(data);
-        },
+      .withConverter(
+        fromFirestore: (snapshot, _) => NoteModel.fromJson(snapshot.data()!),
         toFirestore: (note, _) => note.toJson(),
       );
 
+  // final noteRef = FirebaseFirestore.instance
+  //     .collection("notes")
+  //     .withConverter<NoteModel>(
+  //       fromFirestore: (snapshot, _) {
+  //         final data = snapshot.data()!;
+  //         data["note_id"] ??= snapshot.id;
+  //         return NoteModel.fromJson(data);
+  //       },
+  //       toFirestore: (note, _) => note.toJson(),
+  //     );
+
   /// CREATE
   Future<String> insertItem(NoteModel note) async {
-    final doc = await noteRef.add(note);
+    final doc = await _currentUserRef.add(note);
     await doc.update({"note_id": doc.id});
     return doc.id;
   }
 
   /// READ - GET ALL
   Future<List<NoteModel>> fetchItem() async {
-    final snapshot = await noteRef
+    final snapshot = await _currentUserRef
         .orderBy("created_at", descending: true)
         .get();
     return snapshot.docs.map((doc) => doc.data()).toList();
@@ -30,7 +42,7 @@ class FirestoreHelper {
 
   /// READ - GET BY ID
   Future<NoteModel?> getNoteById(String id) async {
-    final doc = await noteRef.doc(id).get();
+    final doc = await _currentUserRef.doc(id).get();
     return doc.data();
   }
 
@@ -39,17 +51,17 @@ class FirestoreHelper {
     if (note.noteId == null || note.noteId.toString().isEmpty) {
       throw Exception("note_id is required");
     }
-    await noteRef.doc(note.noteId.toString()).set(note);
+    await _currentUserRef.doc(note.noteId.toString()).set(note);
   }
 
   /// UPDATE PARTIAL
   Future<void> updateFields(String id, Map<String, dynamic> fields) async {
-    await noteRef.doc(id).update(fields);
+    await _currentUserRef.doc(id).update(fields);
   }
 
   /// DELETE
   Future<void> deleteItem(String id) async {
-    await noteRef.doc(id).delete();
+    await _currentUserRef.doc(id).delete();
   }
 
   // ---------------------------------------------------------
@@ -58,16 +70,17 @@ class FirestoreHelper {
 
   /// STREAM ALL NOTES (REALTIME)
   Stream<List<NoteModel>> streamAllNotes() {
-    return noteRef.orderBy("created_at", descending: true).snapshots().map((
-      snapshot,
-    ) {
-      return snapshot.docs.map((doc) => doc.data()).toList();
-    });
+    return _currentUserRef
+        .orderBy("created_at", descending: true)
+        .snapshots()
+        .map((snapshot) {
+          return snapshot.docs.map((doc) => doc.data()).toList();
+        });
   }
 
   /// STREAM SINGLE NOTE BY ID (REALTIME)
   Stream<NoteModel?> streamNoteById(String id) {
-    return noteRef.doc(id).snapshots().map((snapshot) {
+    return _currentUserRef.doc(id).snapshots().map((snapshot) {
       if (!snapshot.exists) return null;
       return snapshot.data();
     });
